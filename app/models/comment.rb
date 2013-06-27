275f80526f54cf4c8f3920731608a69d5f00e3f9
u9510606 <u9510606@gmail.com>
2013-06-27 14:26:23 +0800

    little replace

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :adm_user_id, :content, :subject
+  attr_accessible :adm_user_id, :content, :subject, :stage, :handling_adm_user_id, 
+				  :assigning_adm_user_id, :report, :adm_note, :handle_note
   belongs_to :adm_user
 end
