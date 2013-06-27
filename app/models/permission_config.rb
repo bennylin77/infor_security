50ab860b7d5d9589f754f59bdd4829600766ac4d
u9510606 <u9510606@gmail.com>
2013-06-27 14:28:34 +0800

    little replace ++

--- app/models/permission_config.rb
+++ app/models/permission_config.rb
@@ -1,4 +1,4 @@
 class PermissionConfig < ActiveRecord::Base
-  attr_accessible :account, :adm_user_id, :building, :event, :ip, :job
+  attr_accessible :account, :adm_user_id, :building, :event, :ip, :job, :comment
   belongs_to :adm_user  
 end
