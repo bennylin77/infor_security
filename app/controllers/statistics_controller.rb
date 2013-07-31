92cea901c84e4a1f7320746e2e5e934b0e674d16
u9510606 <u9510606@gmail.com>
2013-07-31 22:19:53 +0800

    statistics work v1

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -3,8 +3,15 @@ class StatisticsController < ApplicationController
 def index
 end
 def create
-	@date=params[:d1]
-	@Res=JobDetail.find(:all,:conditions=>['((alert=1 and log_count>=1000) or (alert=0 and log_count>=5)) and updated_at>= ? and updated_at<=?',DateTime.strptime(params[:d1], "%m/%d/%Y %H:%M").to_datetime,DateTime.strptime(params[:d2], "%m/%d/%Y %H:%M").to_datetime])
+	@date=params[:chbox][:category]
+	if params[:chbox][:category]=='0'     #by stage 
+		if params[:chtime][:category]=='1'   #target time
+			@Res=JobDetail.find(:all,:joins=>:job,:conditions=>['((jobs.always_visible=1) or (alert=1 and log_count>=1000) or (alert=0 and log_count>=5)) and jobs.always_handle=1 and job_details.updated_at>= ? and job_details.updated_at<=? ',DateTime.strptime(params[:d1], "%m/%d/%Y %H:%M").to_datetime,DateTime.strptime(params[:d2], "%m/%d/%Y %H:%M").to_datetime])
+		else			#for all
+			@Res=JobDetail.find(:all,:joins=>:job,:conditions=>['((jobs.always_visible=1) or (alert=1 and log_count>=1000) or (alert=0 and log_count>=5)) and jobs.always_handle=1 and src_ip REGEXP ?','.*' ])
+		end	
+	else #by department 	
+	end
     render "showRes"
 end
 
--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -2,16 +2,41 @@ module StatisticsHelper
 
 def count_stage(jobs)
 	
-	s1_count=0
 	
+	params[:st2]=0
+	t_st2=0
+	params[:st3]=0
+	t_st3=0
+	params[:st4]=0
+	t_st4=0
+	params[:st5]=0
+	t_st5=0
+	
+	params[:un] =0	
+	params[:finish]=0
+	
+	##
+	params[:test]=0
+	##
 	jobs.each do |j|
-		if j.job.stage1=='un'
-			s1_count=s1_count+1
-		end	
-	end
-	
-	s1_count
-		
+			if j.job.stage5=='finish'
+				params[:finish]+=1
+			elsif j.job.stage4=='finish'  # on stage5
+				params[:st5] +=1
+			elsif j.job.stage3=='finish'  # on stage4
+				params[:st4] +=1
+			elsif j.job.stage2=='finish'  # on stage3
+				params[:st3] +=1
+			elsif j.job.stage1=='finish'  # on stage2
+				params[:st2] +=1	
+			elsif j.job.stage1=='un'      # on stage2
+				params[:un]  +=1
+			else
+				params[:finish]=params[:finish]  # ?
+			end	
+			
+			params[:test]+=1
+	end		
 end
 
 end
