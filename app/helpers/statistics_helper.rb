26f4bcc2aa22ceb6caad5608ef56042a035f8fa5
u9510606 <u9510606@gmail.com>
2013-08-01 23:00:01 +0800

    statistics work v4 : by stage complete

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -6,7 +6,7 @@ def show_ip(jobs)
 	s_time=0
 	jobs.each do |j|
 		html+='<tr><td>'+j.job_id.to_s+'</td><td>'+j.src_ip.to_s+'</td>'
-		difference=((DateTime.now - j.created_at.to_datetime)*24).to_i.to_s
+		difference=((((DateTime.now - j.created_at.to_datetime)*24).to_i)+8).to_s
 		html+='<td>'+DateTime.now.to_s+'</td>'
 		html+='<td>'+j.created_at.to_datetime.to_s+'</td>'
 		html+='<td>'+difference+'</td>'
@@ -31,6 +31,11 @@ def count_stage(jobs)
 	params[:un] =0	
 	params[:finish]=0
 	
+	params[:avg_st2]=0
+	params[:avg_st3]=0
+	params[:avg_st4]=0
+	params[:avg_st5]=0
+	
 	##
 	params[:test]=0
 	##
@@ -39,20 +44,52 @@ def count_stage(jobs)
 				params[:finish]+=1
 			elsif j.job.stage4=='finish'  # on stage5
 				params[:st5] +=1
+				if j.job.s_check.done_at.to_datetime.blank?
+					t_st5+=0
+				else
+					t_st5+=(((DateTime.now - j.job.s_check.done_at.to_datetime)*24).to_i)+8
+				end
 			elsif j.job.stage3=='finish'  # on stage4
 				params[:st4] +=1
+				t_st4+=(((DateTime.now - j.job.s_handle.done_at.to_datetime)*24).to_i)+8
 			elsif j.job.stage2=='finish'  # on stage3
 				params[:st3] +=1
+				t_st3+=(((DateTime.now - j.job.s_inform.done_at.to_datetime)*24).to_i)+8
 			elsif j.job.stage1=='finish'  # on stage2
 				params[:st2] +=1	
-			elsif j.job.stage1=='un'      # on stage2
+				if j.job.s_assign.done_at.blank?
+					t_st2+=0
+				else	
+					t_st2+=(((DateTime.now - j.job.s_assign.done_at.to_datetime)*24).to_i)+8
+				end
+			elsif j.job.stage1=='un'      # on stage1
 				params[:un]  +=1
 			else
 				params[:finish]=params[:finish]  # ?
 			end	
 			
+			if params[:st2]>0
+				t_st2/=params[:st2]
+				params[:avg_st2]=t_st2
+			end
+			if params[:st3]>0	 		
+				t_st3/=params[:st3]
+				params[:avg_st3]=t_st3
+			end
+			if params[:st4]>0	
+				t_st4/=params[:st4]
+				params[:avg_st4]=t_st4
+			end
+			if params[:st5]>0	
+				t_st5/=params[:st5]
+				params[:avg_st5]=t_st5
+			end
+					
 			params[:test]+=1
 	end		
+	
+	params[:test]
+	
 end
 
 def ip_trans
