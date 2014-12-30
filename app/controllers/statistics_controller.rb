074a20cc720dc5a6eaa1c91df5cd7e349fbcff26
Sun <t6847kimo@gmail.com>
2014-12-30 16:19:14 +0800

    update for Shan Jung

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,7 +2,7 @@ class StatisticsController < ApplicationController
 include StatisticsHelper
 
 def index
-	@Res=JobDetail.find(:all,:joins=>:job,:conditions=>['((alert=1 and log_count>=1000) or (alert=0 and log_count>=5)) and jobs.always_handle=1 and jobs.deleted=0 and job_details.updated_at>= ?',DateTime.now.ago(7.day)])
+	@Res=JobDetail.find(:all,:joins=>:job,:conditions=>['((isflood_scan=1 and log_count>50) or (isflood_scan=0 and alert =0 and log_count>20) or (alert=1 and log_count >= 1000)) and jobs.always_handle=1 and jobs.deleted=0 and job_details.updated_at>= ?',DateTime.now.ago(7.day)])
 	#@serverity=JobThreat.find(:all,
 							  #:select=>'DISTINCT serverity as a')
 	@serverity=Array.new
